f03cb86b5427cd7c5c0c23dd07a601d040b57fe3
nakainu <c011619379@edu.teu.ac.jp>
2019-10-03 17:39:03 +0900

    fixed sphere

--- arkit_sphere/arkit_sphere/ViewController.swift
+++ arkit_sphere/arkit_sphere/ViewController.swift
@@ -36,7 +36,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     // 平面を検出したときに呼ばれる
-    func render(_ render: SCNSceneRenderer, fifAdd node: SCNNode, for anchor: ARAnchor) {
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         // 球のノードを作成
         let sphereNode = SCNNode()
         
@@ -46,48 +46,5 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // 検出面のし子要素にする
         node.addChildNode(sphereNode)
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
-        sceneView.session.run(configuration)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Pause the view's session
-        sceneView.session.pause()
-    }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
     }
 }
